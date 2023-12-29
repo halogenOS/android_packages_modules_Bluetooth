2da8a2c8be2516ea0c3ab5fc0c40eb5f0c0f748a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-29 13:16:31 +0200

    add checks for BLUETOOTH_PRIVILEGED_ANDROID_AUTO permission
    Signed-off-by: Noah Anleitner <noah.anleitner@nonetwor.cc>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- android/app/src/com/android/bluetooth/btservice/AdapterServiceBinder.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterServiceBinder.java
@@ -731,7 +731,13 @@ class AdapterServiceBinder extends IBluetooth.Stub {
             return Collections.emptyList();
         }
 
-        service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        try {
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        } catch (SecurityException e) {
+            /* see android.bluetooth.BluetoothAdapter#getActiveDevices */
+            Utils.enforceBluetoothPrivilegedAndroidAutoOrThrow(service, e);
+        }
+
         return service.getActiveDevices(profile);
     }
 
@@ -1019,7 +1025,12 @@ class AdapterServiceBinder extends IBluetooth.Stub {
             return false;
         }
 
-        service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        try {
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        } catch (SecurityException e) {
+            /* see android.bluetooth.BluetoothDevice#setPairingConfirmation */
+            Utils.enforceBluetoothPrivilegedAndroidAutoOrThrow(service, e);
+        }
 
         DeviceProperties deviceProp = service.getRemoteDevices().getDeviceProperties(device);
         if (deviceProp == null || !deviceProp.isBonding()) {
@@ -1592,7 +1603,13 @@ class AdapterServiceBinder extends IBluetooth.Stub {
             return false;
         }
 
-        service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        try {
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        } catch (SecurityException e) {
+            /* see android.bluetooth.BluetoothDevice#setMetadata */
+            Utils.enforceBluetoothPrivilegedAndroidAutoOrThrow(service, e);
+        }
+
         return service.setMetadata(device, key, value);
     }
 
@@ -1605,7 +1622,13 @@ class AdapterServiceBinder extends IBluetooth.Stub {
             return null;
         }
 
-        service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        try {
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+        } catch (SecurityException e) {
+            /* see android.bluetooth.BluetoothDevice#getMetadata */
+            Utils.enforceBluetoothPrivilegedAndroidAutoOrThrow(service, e);
+        }
+
         return service.getMetadata(device, key);
     }
 
